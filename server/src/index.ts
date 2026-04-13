e5a8c2e93bfbec59588f596ddd1849cb62586690
Harvey Punsalan <punsalanharvey019@gmail.com>
2026-04-13 15:50:53 +0800

    feat: creating my router

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import cors from 'cors';
+import router from './routes/auth'
 
 
 const app = express()
@@ -7,6 +8,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
+app.use('/auth', router)
 
 
 app.get('/health', (req, res) => res.json({ status: 'ok' }));
