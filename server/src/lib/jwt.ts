1c1be955ad9ea5a78dcd74034fb8879175ddd4c2
Harvey Punsalan <punsalanharvey019@gmail.com>
2026-04-14 14:30:18 +0800

    feat: install octokit, jsonwebtoken and add jwt utility functions

---
+++ server/src/lib/jwt.ts
@@ -0,0 +1,14 @@
+import jwt from 'jsonwebtoken';
+
+export function signToken(userId: string): string {
+  return jwt.sign({ userId }, process.env.JWT_SECRET!, { expiresIn: '7d' });
+};
+
+export function verifyToken(token: string): { userId: string } {
+  return jwt.verify( token , process.env.JWT_SECRET!) as { userId: string };
+};
+
+
+
+
+
