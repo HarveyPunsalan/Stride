46c51932424692db5a368d2083609fe866df5147
Harvey Punsalan <punsalanharvey019@gmail.com>
2026-04-22 14:09:06 +0800

    feat: set up React Router with 7 page routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Activity from "./pages/Activity";
+import AiCoach from "./pages/AiCoach";
+import Dashboard from "./pages/Dashboard";
+import Landing from "./pages/Landing";
+import PublicProfile from "./pages/PublicProfile";
+import Repositories from "./pages/Repositories";
+import Settings from "./pages/Settings";
 
 function App() {
-  
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/activity" element={<Activity />} />
+        <Route path="/aiCoach" element={<AiCoach />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/" element={<Landing />} />
+        <Route path="/publicProfile" element={<PublicProfile />} />
+        <Route path="/repositories" element={<Repositories />} />
+        <Route path="/settings" element={<Settings />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
