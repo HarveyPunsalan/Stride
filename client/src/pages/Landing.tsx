c55ca7e5304810c66dd116549075460a7a74a2fe
Harvey Punsalan <punsalanharvey019@gmail.com>
2026-04-23 13:11:08 +0800

    setting up the bone structure of my landing

--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -1,5 +1,14 @@
-function Landing() {
-  return <div>Landing</div>;
-}
+export default function Landing() {
+  const handleLogin = () => {
+    // redirect to VITE_API_URL/auth/github
+    window.location.href = import.meta.env.VITE_API_URL + "/auth/github";
+    
+  }
+  return (
+    <div>
+      <h1>howcase your real GitHub activity not just your resume</h1>
+      <button onClick={handleLogin}>Connect with GitHub</button>
+    </div>
 
-export default Landing;
+  );
+}
