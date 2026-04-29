d403078927246cd8ac8204e73bf8c7c7515d6b42
Harvey Punsalan <punsalanharvey019@gmail.com>
2026-04-29 12:03:13 +0800

    feat: created src/lib/api.ts and create an api helper function

---
+++ client/src/lib/api.ts
@@ -0,0 +1,16 @@
+export async function apiFetch<T>(path: string, options?: RequestInit): Promise<T> {
+    const token = localStorage.getItem("devboard_token");
+    const res = await fetch(`${import.meta.env.VITE_API_URL}${path}`, {
+        ...options,
+        headers: {
+            ...options?.headers,
+            Authorization: `Bearer ${token}`,
+        }
+    })
+
+    if (res.status === 401) {
+        throw new Error("Unauthorized")
+    }
+
+    return res.json() as Promise<T>
+}
